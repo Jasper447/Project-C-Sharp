4fc7773bdcac2f46e0f07edd0e5a39455bb82c2e
Jepjur <Jasper_vandijk@hotmail.com>
2020-03-23 09:46:48 +0100

    Add project files.

---
+++ Bio/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Bio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Opdracht 1.1
+            string name = "Jasper van Dijk";
+            string address = "Oosterstraat 91";
+            string postalCode = "7531TG";
+            string age = "28";
+
+            Console.WriteLine($"{name} \n{address} \n{postalCode} \n{age}");
+
+            // Opdracht 1.2
+            Console.WriteLine($"********************* \n***{name}***\n*********************");
+        }
+    }
+}
