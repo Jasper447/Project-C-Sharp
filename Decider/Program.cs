f1b682b1375be14a95d908cb3248eab221198b1d
Jepjur <Jasper_vandijk@hotmail.com>
2020-03-25 17:05:44 +0100

    added 4 projects

--- Decider/Program.cs
+++ Decider/Program.cs
@@ -6,7 +6,18 @@ namespace Decider
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rand = new Random();
+            int roll = rand.Next(10);
+            Console.WriteLine("What is your yes or no question?");
+            Console.ReadLine();
+            if (roll < 5)
+            {
+                Console.WriteLine("Most certainly yes");
+            }
+            else
+            {
+                Console.WriteLine("All signs point to no");
+            }
         }
     }
 }
