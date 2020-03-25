b58022789396549d93bdd0dec58fd07f07c9a0ea
Jasper447 <62293715+Jasper447@users.noreply.github.com>
2020-03-25 15:00:16 +0100

    Update Program.cs

--- Decider/Program.cs
+++ Decider/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Decider
 {
@@ -6,7 +6,19 @@ namespace Decider
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your yes or no question?!");
+            Console.ReadLine();
+            Random rand = new Random();
+            int roll = rand.Next(10);
+            if (roll < 5)
+            {
+                Console.WriteLine("Most certainly yes");
+            }
+            else
+            {
+                Console.WriteLine("All signs point to no");
+            }
+
         }
     }
 }
